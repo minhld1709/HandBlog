d98ed0ac10e8f60defc2e2d4f4deacadb7bce409
minhld1709 <minhld1709@gmail.com>
2016-09-16 16:38:50 +0700

    continue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Category.create name: "AngularJS", sort_name: "AJS"
+Category.create name: "NodeJS", sort_name: "NJS"
+Category.create name: "ReactJS", sort_name: "RJS"
+Category.create name: "Bootstrap Template", sort_name: "BT"
+Category.create name: "Only HTML & CSS", sort_name: "OHC"
+Category.create name: "SemanticUI Template", sort_name: "ST"
+
+User.create name: "Le Dinh Minh", email: "ledinhminh@gmail.com", password: "123123123", password_confirmation: "123123123"
+User.create name: "Nguyen Thanh Tung", email: "tung@gmail.com", password: "123123123", password_confirmation: "123123123"
