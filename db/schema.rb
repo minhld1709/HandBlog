04b29a26776ce6c91f05c3c7d502af48c3e47dc0
minhld1709 <minhld1709@gmail.com>
2016-09-17 17:25:54 +0700

    comment edit

--- db/schema.rb
+++ db/schema.rb
@@ -22,10 +22,11 @@ ActiveRecord::Schema.define(version: 20160916181228) do
   create_table "comments", force: :cascade do |t|
     t.integer  "project_id", null: false
     t.integer  "own_id",     null: false
-    t.integer  "comment_id"
+    t.integer  "cm_id"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
   end
 
   create_table "projects", force: :cascade do |t|
